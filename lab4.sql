624819eefe367c79d7111ee88b4c2b6c7af0865c
poslam <kirden04@gmail.com>
2023-11-18 10:30:32 +1000

    lesson

--- lab4.sql
+++ lab4.sql
@@ -62,3 +62,30 @@ order by "date", shop_ref
 
 # 4
 
+
+
+
+
+
+
+
+
+# lesson
+select *,
+       count(*) over(partition by "date") as cnt
+from invoice
+order by "date"
+
+# скользящая сумма
+
+with "money" as (
+    select "date",
+           shop_ref,
+           price_per_one * amount as "invoice_money"
+    from invoice
+)
+select "date",
+       shop_ref,
+       invoice_money,
+       sum(invoice_money) over(partition by shop_ref order by "date") as "run_sum"
+from "money"
