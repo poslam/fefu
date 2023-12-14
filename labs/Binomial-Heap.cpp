aeb8478f71a5e5f1ef4c1a2d0d7a7a7f6a4917d7
poslam <kirden04@gmail.com>
2023-12-14 12:04:41 +1000

    formatting

--- labs/Binomial-Heap.cpp
+++ labs/Binomial-Heap.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data, degree;
     Node *child, *sibling, *parent;
 };
 
-Node *newNode(int key) {
+Node *newNode(int key)
+{
     Node *temp = new Node;
     temp->data = key;
     temp->degree = 0;
@@ -15,14 +17,50 @@ Node *newNode(int key) {
     return temp;
 }
 
+list<Node *> unionBionomialHeap(list<Node *> l1,
+                                list<Node *> l2)
+{
+    list<Node *> _new;
+    list<Node *>::iterator it = l1.begin();
+    list<Node *>::iterator ot = l2.begin();
+
+    while (it != l1.end() && ot != l2.end())
+    {
+        // if D(l1) <= D(l2)
+        if ((*it)->degree <= (*ot)->degree)
+        {
+            _new.push_back(*it);
+            it++;
+        }
+        // if D(l1) > D(l2)
+        else
+        {
+            _new.push_back(*ot);
+            ot++;
+        }
+    }
+
+    while (it != l1.end())
+    {
+        _new.push_back(*it);
+        it++;
+    }
+
+    while (ot != l2.end())
+    {
+        _new.push_back(*ot);
+        ot++;
+    }
+    return _new;
+}
+
 // This function merge two Binomial Trees.
-Node *mergeBinomialTrees(Node *b1, Node *b2) {
-// Make sure b1 is smaller
+Node *mergeBinomialTrees(Node *b1, Node *b2)
+{
+    // Make sure b1 is smaller
     if (b1->data > b2->data)
         swap(b1, b2);
 
-// We basically make larger valued tree
-// a child of smaller valued tree
     b2->parent = b1;
     b2->sibling = b1->child;
     b1->child = b2;
@@ -31,58 +69,33 @@ Node *mergeBinomialTrees(Node *b1, Node *b2) {
     return b1;
 }
 
-list<Node *> unionBionomialHeap(list<Node *> l1,
-                                list<Node *> l2) {
-    list<Node *> _new;
-    list<Node *>::iterator it = l1.begin();
-    list<Node *>::iterator ot = l2.begin();
-    
-    while (it != l1.end() && ot != l2.end()) {
-        // if D(l1) <= D(l2)
-        if ((*it)->degree <= (*ot)->degree) {
-            _new.push_back(*it);
-            it++;
-        }
-            // if D(l1) > D(l2)
-        else {
-            _new.push_back(*ot);
-            ot++;
-        }
-    }
-
-    while (it != l1.end()) {
-        _new.push_back(*it);
-        it++;
-    }
-
-    while (ot != l2.end()) {
-        _new.push_back(*ot);
-        ot++;
-    }
-    return _new;
-}
-
-list<Node *> adjust(list<Node *> _heap) {
+list<Node *> adjust(list<Node *> _heap)
+{
     if (_heap.size() <= 1)
         return _heap;
     list<Node *> new_heap;
     list<Node *>::iterator it1, it2, it3;
     it1 = it2 = it3 = _heap.begin();
 
-    if (_heap.size() == 2) {
+    if (_heap.size() == 2)
+    {
         it2 = it1;
         it2++;
         it3 = _heap.end();
-    } else {
+    }
+    else
+    {
         it2++;
         it3 = it2;
         it3++;
     }
-    while (it1 != _heap.end()) {
+    while (it1 != _heap.end())
+    {
         if (it2 == _heap.end())
             it1++;
 
-        else if ((*it1)->degree < (*it2)->degree) {
+        else if ((*it1)->degree < (*it2)->degree)
+        {
             it1++;
             it2++;
             if (it3 != _heap.end())
@@ -91,14 +104,16 @@ list<Node *> adjust(list<Node *> _heap) {
 
         else if (it3 != _heap.end() &&
                  (*it1)->degree == (*it2)->degree &&
-                 (*it1)->degree == (*it3)->degree) {
+                 (*it1)->degree == (*it3)->degree)
+        {
             it1++;
             it2++;
             it3++;
         }
 
-            // if degree of two Binomial Tree are same in heap
-        else if ((*it1)->degree == (*it2)->degree) {
+        // if degree of two Binomial Tree are same in heap
+        else if ((*it1)->degree == (*it2)->degree)
+        {
             Node *temp;
             *it1 = mergeBinomialTrees(*it1, *it2);
             it2 = _heap.erase(it2);
@@ -111,26 +126,29 @@ list<Node *> adjust(list<Node *> _heap) {
 
 // inserting a Binomial Tree into binomial heap
 list<Node *> insertATreeInHeap(list<Node *> _heap,
-                               Node *tree) {
-// creating a new heap i.e temp
+                               Node *tree)
+{
+    // creating a new heap i.e temp
     list<Node *> temp;
 
-// inserting Binomial Tree into heap
+    // inserting Binomial Tree into heap
     temp.push_back(tree);
 
-// perform union operation to finally insert
-// Binomial Tree in original heap
+    // perform union operation to finally insert
+    // Binomial Tree in original heap
     temp = unionBionomialHeap(_heap, temp);
 
     return adjust(temp);
 }
 
-list<Node *> removeMinFromTreeReturnBHeap(Node *tree) {
+list<Node *> removeMinFromTreeReturnBHeap(Node *tree)
+{
     list<Node *> heap;
     Node *temp = tree->child;
     Node *lo;
 
-    while (temp) {
+    while (temp)
+    {
         lo = temp;
         temp = temp->sibling;
         lo->sibling = NULL;
@@ -139,15 +157,18 @@ list<Node *> removeMinFromTreeReturnBHeap(Node *tree) {
     return heap;
 }
 
-list<Node *> insert(list<Node *> _head, int key) {
+list<Node *> insert(list<Node *> _head, int key)
+{
     Node *temp = newNode(key);
     return insertATreeInHeap(_head, temp);
 }
 
-Node *getMin(list<Node *> _heap) {
+Node *getMin(list<Node *> _heap)
+{
     list<Node *>::iterator it = _heap.begin();
     Node *temp = *it;
-    while (it != _heap.end()) {
+    while (it != _heap.end())
+    {
         if ((*it)->data < temp->data)
             temp = *it;
         it++;
@@ -155,15 +176,18 @@ Node *getMin(list<Node *> _heap) {
     return temp;
 }
 
-list<Node *> extractMin(list<Node *> _heap) {
+list<Node *> extractMin(list<Node *> _heap)
+{
     list<Node *> new_heap, lo;
     Node *temp;
 
     temp = getMin(_heap);
     list<Node *>::iterator it;
     it = _heap.begin();
-    while (it != _heap.end()) {
-        if (*it != temp) {
+    while (it != _heap.end())
+    {
+        if (*it != temp)
+        {
             new_heap.push_back(*it);
         }
         it++;
@@ -175,8 +199,10 @@ list<Node *> extractMin(list<Node *> _heap) {
 }
 
 // print function for Binomial Tree
-void printTree(Node *h) {
-    while (h) {
+void printTree(Node *h)
+{
+    while (h)
+    {
         cout << h->data << " ";
         printTree(h->child);
         h = h->sibling;
@@ -184,16 +210,19 @@ void printTree(Node *h) {
 }
 
 // print function for binomial heap
-void printHeap(list<Node *> _heap) {
+void printHeap(list<Node *> _heap)
+{
     list<Node *>::iterator it;
     it = _heap.begin();
-    while (it != _heap.end()) {
+    while (it != _heap.end())
+    {
         printTree(*it);
         it++;
     }
 }
 
-int main() {
+int main()
+{
     list<Node *> _heap;
 
     _heap = insert(_heap, 10);
